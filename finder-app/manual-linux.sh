2dfeeeb28194ee0f5a6924f93fcb5f3e551fe231
JithendraHS <jihs6098@colorado.edu>
2024-07-21 13:04:01 -0600

    Migration

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -22,8 +22,10 @@ else
 fi
 
 mkdir -p ${OUTDIR}
-
 cd "$OUTDIR"
+OUTDIR=$(realpath $(dirname $0))
+: <<'END'
+
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
@@ -35,9 +37,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    git apply ${FINDER_APP_DIR}/mypatch.patch
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all -j4
+    #make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/arm64/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,33 +57,75 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -pv rootfs/{bin,dev,home,lib,lib64,sbin,tmp,var/log,etc,proc,sys,usr/{bin,sbin,lib}}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
+cd ${OUTDIR}/rootfs
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 lib/
+cp /usr/aarch64-linux-gnu/lib/libm.so.6 lib64/
+cp /usr/aarch64-linux-gnu/lib/libc.so.6 lib64/
+cp /usr/aarch64-linux-gnu/lib/libresolv.so.2 lib64/
 
 # TODO: Make device nodes
-
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 660 dev/console c 5 1
+END
 # TODO: Clean and build the writer utility
-
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cd ${FINDER_APP_DIR}
+make clean && make && cp writer finder.sh finder-test.sh autorun-qemu.sh ${OUTDIR}/rootfs/home && cp -r ../conf ${OUTDIR}/rootfs/home/conf
+
+sudo cat << 'EOF' > ${OUTDIR}/rootfs/init
+#!/bin/sh
+
+echo "Starting init script..."
+# Mount necessary filesystems
+mount -t proc proc /proc
+if [ $? -ne 0 ]; then
+    echo "Failed to mount proc"
+    exec /bin/sh
+fi
+
+mount -t sysfs sysfs /sys
+if [ $? -ne 0 ]; then
+    echo "Failed to mount sysfs"
+    exec /bin/sh
+fi
+
+# Print success message and run shell
+echo "Init script executed successfully"
+exec /bin/sh
+EOF
+
+chmod +x ${OUTDIR}/rootfs/init
 
 # TODO: Chown the root directory
+cd ${OUTDIR}
+sudo chown root:root rootfs
 
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
